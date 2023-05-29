504867a45c6e0ed9fb73f2f4c680619f6125384f
Shafi Abdullah <104518333+C-loud-Nine@users.noreply.github.com>
2023-05-30 00:06:34 +0600

    Update circularlinkedlist.cpp

--- circularlinkedlist.cpp
+++ circularlinkedlist.cpp
@@ -84,6 +84,94 @@ void deletebegin(node** head){
     }
 }
 
+bool isEmpty(node* head) {
+    return head == NULL;
+}
+
+int length(node* head) {
+    if (head == NULL) {
+        return 0;
+    }
+    int count = 0;
+    node* current = head;
+    do {
+        count++;
+        current = current->link;
+    } while (current != head);
+    return count;
+}
+
+void insertAfter(node* head, int position, int data) {
+    int len = length(head);
+    if (position < 1 || position > len + 1) {
+        cout << "Invalid position!" << endl;
+        return;
+    }
+
+    node* newNode = new node();
+    newNode->data = data;
+
+    if (position == 1) {
+        newNode->link = head->link;
+        head->link = newNode;
+    } else {
+        node* current = head;
+        for (int i = 1; i < position; i++) {
+            current = current->link;
+        }
+        newNode->link = current->link;
+        current->link = newNode;
+    }
+}
+
+void deleteAtPosition(node** head, int position) {
+    int len = length(*head);
+    if (position < 1 || position > len) {
+        cout << "Invalid position!" << endl;
+        return;
+    }
+
+    if (position == 1) {
+        node* last = *head;
+        while (last->link != *head) {
+            last = last->link;
+        }
+        node* temp = *head;
+        *head = (*head)->link;
+        last->link = *head;
+        delete temp;
+    } else {
+        node* current = *head;
+        for (int i = 1; i < position - 1; i++) {
+            current = current->link;
+        }
+        node* temp = current->link;
+        current->link = temp->link;
+        delete temp;
+    }
+}
+
+void reverseList(node** head) {
+    if (*head == NULL) {
+        return;
+    }
+
+    node* prev = NULL;
+    node* current = *head;
+    node* next = NULL;
+
+    do {
+        next = current->link;
+        current->link = prev;
+        prev = current;
+        current = next;
+    } while (current != *head);
+
+    (*head)->link = prev;
+    *head = prev;
+}
+
+
 void delend(node** head){
     if(*head==NULL){
         cout<<"EMPTY"<<endl;
